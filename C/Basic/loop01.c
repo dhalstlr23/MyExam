4aae0ec7becbb758624337b76c8c8f71cf7ed1e2
dhalstlr23 <65219523+dhalstlr23@users.noreply.github.com>
2025-07-21 15:12:38 +0900

    커멘드는 수정가능
    커멘드는 수정 가능

---
+++ C/Basic/loop01.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main()
+{
+	int i = 100;
+
+	while (i >= 0) {
+		printf("%d\n", i--);
+		//i--;
+		// i -= 1;
+		// i = i - 1;
+	}
+
+	return 0;
+
+}
